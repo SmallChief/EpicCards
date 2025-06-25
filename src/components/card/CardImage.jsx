971b4caa04f468bf59b14f046d2b40d452e9370c
SmallChief <henning.ukena@gmx.de>
2025-06-25 21:16:24 +0200

    Implement first draft of drag-and-drop functionality for CardImage component

--- src/components/card/CardImage.jsx
+++ src/components/card/CardImage.jsx
@@ -1,6 +1,43 @@
 import "./CardImage.css";
+import { useEffect, useRef, useState } from "react";
 
 function CardImage({ image, onImageChange }) {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [dragging, setDragging] = useState(false);
+  const dragStart = useRef({ x: 0, y: 0 });
+
+  const handleMouseDown = (e) => {
+    setDragging(true);
+    dragStart.current = { x: e.clientX, y: e.clientY };
+  };
+
+  const handleMouseMove = (e) => {
+    if (dragging) {
+      const dx = e.clientX - dragStart.current.x;
+      const dy = e.clientY - dragStart.current.y;
+      setPosition({ x: position.x + dx, y: position.y + dy });
+    }
+  };
+
+  const handleMouseUp = () => {
+    setDragging(false);
+  };
+
+  useEffect(() => {
+    if (dragging) {
+      window.addEventListener("mousemove", handleMouseMove);
+      window.addEventListener("mouseup", handleMouseUp);
+    } else {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    }
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+      window.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [dragging]);
+
   if (!image) {
     return (
       <div className="card__image">
@@ -18,7 +55,14 @@ function CardImage({ image, onImageChange }) {
   } else {
     return (
       <div className="card__image">
-        <img src={image} alt="Card" className="card__image-content" />
+        <img
+          src={image}
+          style={{ left: position.x, top: position.y }}
+          onMouseDown={handleMouseDown}
+          alt="Card"
+          className="card__image-content"
+          draggable="false"
+        />
       </div>
     );
   }
