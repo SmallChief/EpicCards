1fb8d55521c819dffdea878a504f6cca53d803a9
SmallChief <henning.ukena@gmx.de>
2025-06-25 14:29:17 +0200

    - Add Workspace component to App - Add Card component to App temporarily

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import Menu from "./menu/Menu.jsx";
+import Workspace from "./workspace/Workspace.jsx";
+import Card from "./card/Card.jsx";
 
 function App() {
   return (
     <>
       <Menu />
+      <Workspace>
+        <Card />
+      </Workspace>
     </>
   );
 }
