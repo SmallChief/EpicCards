30447174f4da164c417c20d602e86b4617fa89ce
SmallChief <henning.ukena@gmx.de>
2025-07-02 10:50:15 +0200

    Remove delete button functionality from Workspace component

--- src/components/workspace/Workspace.jsx
+++ src/components/workspace/Workspace.jsx
@@ -1,16 +1,7 @@
 import "./Workspace.css";
 
-function Workspace({ children, onDeleteCard, showDeleteButton }) {
-  return (
-    <div className="workspace">
-      {children}
-      {showDeleteButton && (
-        <button className="workspace__delete-btn" onClick={onDeleteCard}>
-          Delete Card
-        </button>
-      )}
-    </div>
-  );
+function Workspace({ children }) {
+  return <div className="workspace">{children}</div>;
 }
 
 export default Workspace;
